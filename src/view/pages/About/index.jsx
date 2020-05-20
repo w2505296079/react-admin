a80a1948003fb0e8bc29782cd5f0a6323b401955
yiren_wei <2505296079@qq.com>
2020-05-20 23:18:33 +0800

    路由修改

--- src/view/pages/About/index.jsx
+++ src/view/pages/About/index.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import './index.module.scss';
+import React from 'react'
+import styles from './index.module.scss'
+
+import { withRouter } from 'react-router-dom'
 
 class About extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {};
+    super(props)
+    this.state = {}
+    console.log(this.props)
+  }
+  toHome() {
+    console.log('ss')
+    this.props.history.replace('/')
   }
 
   render() {
-    return <div>About</div>;
+    return (
+      <div className={styles.content} onClick={() => this.toHome()}>
+        About
+      </div>
+    )
   }
 }
 
-export default About;
+export default withRouter(About)
